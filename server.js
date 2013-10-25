1ec4a21d241326c958a880b13d445995ff7593ce
Nick Hilton <nick@leocrotta.csh.rit.edu>
2013-10-25 13:31:06 -0400

    Started playing with mongodb. Currently, this will count how many times the website is visited, and will display the ip's of those who have visited. I'm now at a point where I know how to actually make this blog.

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 var http = require('http');
+var mongo = require('mongojs');
+var db=mongo.connect('mongodb://0.0.0.0/blog', ["testData"]);
+var counter = 0;
+db.testData.remove();
+
 http.createServer(function(request,response) {
-	response.writeHead(200,{'Content-Type':'text/plain'});
-	response.end("Hello world! Test!\n");
+	counter+=1;
+	response.writeHead(200,{'Content-Type':'text/html'});
+	response.write('<html><head></head><body>');
+	var ipAddress = request.connection.remoteAddress;
+	db.testData.save({ip: ipAddress, count: counter}, function(err,val) {
+		if(err) throw err;
+		response.write("<h1>Hello world!</h1>\n");
+		response.write("<table>");
+		var items=db.testData.find();
+		items.forEach(function(err,item) {
+			if(item) {
+				response.write("<tr>");
+				response.write("<td>"+item.count+"</td>");
+				response.write("<td>"+item.ip+'</td>');
+				response.write("</tr>");
+			}
+			else 
+				response.end('<table></body></html>');
+		});
+	});
 }).listen(80);
+
 console.log("Server started");
