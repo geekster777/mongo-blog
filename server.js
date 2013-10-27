74c39e2b29a97490285e21725345a7c1756a423d
Nick Hilton <hilton@csh.rit.edu>
2013-10-26 21:55:11 -0400

    Now only adds one IP to the table, and counts. Also, changed the order in which it works to make for a better response time while friends try to break my server.

--- server.js
+++ server.js
@@ -1,30 +1,53 @@
+/**
+ * Server file, controls the incoming http request, then determines 
+ * where to direct it to receive a response.
+ * Nick Hilton
+ */
+//include server
 var http = require('http');
+
+//connects to the database
 var mongo = require('mongojs');
 var db=mongo.connect('mongodb://0.0.0.0/blog', ["testData"]);
-var counter = 0;
+
+//clear the database when the server is restarted
 db.testData.remove();
 
+//creates the server to listen for incoming requests
 http.createServer(function(request,response) {
-	counter+=1;
+	
+	//writes that the request was received and valid
 	response.writeHead(200,{'Content-Type':'text/html'});
 	response.write('<html><head></head><body>');
-	var ipAddress = request.connection.remoteAddress;
-	db.testData.save({ip: ipAddress, count: counter}, function(err,val) {
-		if(err) throw err;
-		response.write("<h1>Hello world!</h1>\n");
-		response.write("<table>");
-		var items=db.testData.find();
-		items.forEach(function(err,item) {
-			if(item) {
-				response.write("<tr>");
-				response.write("<td>"+item.count+"</td>");
-				response.write("<td>"+item.ip+'</td>');
-				response.write("</tr>");
-			}
-			else 
-				response.end('<table></body></html>');
-		});
+
+	//writes the ip's of all previous visitors
+	response.write("<h1>Hello world!</h1>\n");
+	response.write("<table>");
+	var items=db.testData.find();
+	items.forEach(function(err,item) {
+		if(item) {
+			response.write("<tr>");
+			response.write("<td>"+item.count+"</td>");
+			response.write("<td>"+item.ip+'</td>');
+			response.write("</tr>");
+		}
+		else
+			response.end('</table></body></html>');
 	});
+
+	//finds the user's ip, and adds it to the database
+	var ipAddress = request.connection.remoteAddress;
+	
+	db.testData.find({ip: ipAddress}).count( function(err, count) {
+		if(count==0)
+			db.testData.save({ip: ipAddress, count: 0}, function(err,val) {
+				if(err) throw err;
+			});
+		else {
+			db.testData.update({ip: ipAddress}, {$inc : { count: 1}});
+		}
+	});
+		
 }).listen(80);
 
 console.log("Server started");
