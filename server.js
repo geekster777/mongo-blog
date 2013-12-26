cda66f5298f358c72b9f2c79f8fc5264183c58b1
Nick Hilton <hilton@csh.rit.edu>
2013-12-26 00:40:13 -0500

    Server can now recursively hande includes within files. This means I have to start checking to see if file1 includes file2 which includes file1. For now, I still need to work on adding content to the database. I am seeing progress, though. Will be done soonish.

--- server.js
+++ server.js
@@ -17,8 +17,45 @@ var fs = require('fs');
 //clear the database when the server is restarted
 db.testData.remove();
 
-//Checks the database for the existence of a page, and will return its data
-function lookupPage(pageName, db) {
+/**
+ * Will replace all instances of include statements with the actual file
+ * that is to be included. Include statements are represented as:
+ *    <!-- #include page.html -->
+ * @param:
+ * page: String - the content of the page
+ */
+function includeTemplates(page) {
+
+  //Finds any instance of a commented include statement with one argument
+  var includePatt = /<!--#include\s+\S+\s*-->/m;
+  while( includePatt.test(page) ) {
+    //gets the filename
+    var pos = includePatt.lastIndex;
+    var file = includePatt.source.replace(/<!--#include|-->/,'');
+    file = file.trim();
+    
+    //looks up the file in the database
+    var contents = lookupPage(file);
+    
+    if(contents === null) {
+      contents = fs.readFileSync(file).toString();
+      if(contents===null)
+        continue;
+    }
+    
+    //inserts the include into the content
+    page.replace(includePatt.source,contents);
+  }
+
+  return page;
+}
+
+/** 
+ * Checks the database for the existence of a page, and will return its data
+ * @param  
+ *  pageName: String - the name of the page being looked up in the database
+ */
+function lookupPage(pageName) {
 	if(pageName===null)
 		return "";
 	
@@ -31,31 +68,34 @@ function lookupPage(pageName, db) {
 		page+=item.content;
 	});
   
+  includeTemplates(page, db);
+
+  return page;
 }
 
 //creates the server to listen for incoming requests
 http.createServer(function(request,response) {
-    var url = urlTool.parse(request.url);
-    var page = lookupPage(url.pathname);
+  var url = urlTool.parse(request.url);
+  var page = lookupPage(url.pathname);
 
-    if(page===null){
+  if(page===null){
 
-        //will look up the file in the filesystem if it is not found.
-        fs.readFile(__dirname+url.pathname,function(err, data) {
-            if(!err) {
-                response.write(200);
-                response.write(data);
-            }
-            else
-                response.writeHead(404);
-                response.write("404 not found!");
-        });
-    }
-    else {
-        response.writeHead(200);
-        response.write(page);
-    }
-
+    //will look up the file in the filesystem if it is not found.
+    fs.readFile(__dirname+url.pathname,function(err, data) {
+      if(!err) {
+        response.write(200);
+        response.write(data);
+      }
+      else {
+        response.writeHead(404);
+        response.write("404 not found!");
+      }
+    });
+  }
+  else {
+    response.writeHead(200);
+    response.write(page);
+  }
 
 }).listen(8080);
 
