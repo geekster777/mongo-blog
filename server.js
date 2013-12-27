ffd68b1ba70f6f7a31851c7bd17b3b9582923e5c
Nick Hilton <hilton@csh.rit.edu>
2013-12-27 06:02:14 -0500

    You can now save new pages to the database. Also, the page lookup has been changed to be asynchronous. It's a little more confusing so I'll add better comments explaining it soon. I have yet to test includes in web pages. I'll test that soon. This is basically as bare bones of a content management system as you can get. The back-end is basically done, and the front end needs to be fancied up, but the hard part is done. I just need to add an API, test (and probably fix) the includes system, and set it so it backs up the pages.

--- server.js
+++ server.js
@@ -10,12 +10,13 @@ var http = require('http');
 
 //connects to the database
 var mongo = require('mongojs');
-var db=mongo.connect('mongodb://0.0.0.0/blog', ["pages","templates","users","comments"]);
+var db=mongo.connect('mongodb://0.0.0.0/blog', ["pages","users","comments"]);
 var urlTool = require('url');
 var fs = require('fs');
-
+var qs = require('querystring');
+var __contentdir = __dirname+"/content";
 //clear the database when the server is restarted
-db.testData.remove();
+//db.pages.remove();
 
 /**
  * Will replace all instances of include statements with the actual file
@@ -24,30 +25,33 @@ db.testData.remove();
  * @param:
  * page: String - the content of the page
  */
-function includeTemplates(page) {
+function includeTemplates(page, callback) {
 
   //Finds any instance of a commented include statement with one argument
   var includePatt = /<!--#include\s+\S+\s*-->/m;
-  while( includePatt.test(page) ) {
+  if( includePatt.test(page) ) {
+    console.log("TEMPLATE: "+includePatt.source);
     //gets the filename
     var pos = includePatt.lastIndex;
     var file = includePatt.source.replace(/<!--#include|-->/,'');
     file = file.trim();
     
     //looks up the file in the database
-    var contents = lookupPage(file);
-    
-    if(contents === null) {
-      contents = fs.readFileSync(file).toString();
-      if(contents===null)
-        continue;
-    }
-    
-    //inserts the include into the content
-    page.replace(includePatt.source,contents);
+    lookupPage(file, function(contents) {
+      
+      if(contents === "") {
+        contents = fs.readFileSync(__contentdir+file).toString();
+        if(contents==="")
+          return;
+      }
+      
+      //inserts the include into the content
+      page = page.replace(includePatt.source,contents);
+      includeTemplates(page, callback);
+    });
   }
 
-  return page;
+  callback(page);
 }
 
 /** 
@@ -55,50 +59,84 @@ function includeTemplates(page) {
  * @param  
  *  pageName: String - the name of the page being looked up in the database
  */
-function lookupPage(pageName) {
-	if(pageName===null)
-		return "";
+function lookupPage(pageName, callback) {
+	if(pageName==null)
+		return callback("");
 	
 	var page = "";
 	
 	//looks up the page in the database, 
-	var results=db.pages.find({name: pageName});
-	results.forEach(function(err,item) {
-		page="";
-		page+=item.content;
-	});
-  
-  includeTemplates(page, db);
+	console.log("pageName: "+pageName);
+  db.pages.findOne({_id: pageName}, function(err, result) {
+    page="";
+    if(err || result===null)
+      callback("");
+    else {
+      console.log(result);
+      page+=result.content;
+      includeTemplates(page, callback);
+    }
+  });
 
-  return page;
 }
 
 //creates the server to listen for incoming requests
 http.createServer(function(request,response) {
   var url = urlTool.parse(request.url);
-  var page = lookupPage(url.pathname);
+  console.log(url.pathname); 
+  if(request.method == "GET") {
 
-  if(page===null){
-
-    //will look up the file in the filesystem if it is not found.
-    fs.readFile(__dirname+url.pathname,function(err, data) {
-      if(!err) {
-        response.write(200);
-        response.write(data);
+    lookupPage(url.pathname, function(page) {
+      if(page===""){
+        console.log(__contentdir+url.pathname);
+        //will look up the file in the filesystem if it is not found in the db.
+        fs.readFile(__contentdir+url.pathname,function(err, data) {
+          if(!err) {
+            response.writeHead(200);
+            response.end(data);
+          }
+          else {
+            response.writeHead(404);
+            response.end("404 not found!");
+          }
+        });
       }
       else {
-        response.writeHead(404);
-        response.write("404 not found!");
+        response.writeHead(200);
+        response.end(page);
       }
     });
-  }
-  else {
-    response.writeHead(200);
-    response.write(page);
-  }
 
+  }
+  else if(request.method == "POST") {
+    var requestBody = "";
+    
+    //reads all of the data from the post
+    request.on('data', function(data) {
+      requestBody+=data;
+    });
+
+    request.on('end', function() {
+      var formData = qs.parse(requestBody);
+      
+      console.log(formData.pagename+"!");
+      
+      db.pages.save({_id:"/"+formData.pagename, content:formData.content});
+      db.pages.findOne({_id:"/"+formData.pagename}, function(err, result) {
+        
+        if(err) {
+          response.writeHead(213, {'Content-Type': 'text/html'});
+          response.end("<html><body><h1>Error Saving.</h1></body></html>");
+          return;
+        }
+        
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(result.content+"!!!"+result._id);
+        console.log(result.content+"!!!"+result._id);
+      });
+    });
+  }
 }).listen(8080);
 
 console.log("Server started");
-
 
